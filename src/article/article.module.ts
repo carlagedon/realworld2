713f0503e21dc060cd0705bd8e3ddde07791a8e7
carlagedon <mukanelnur0@gmail.com>
2022-10-04 22:56:19 +0600

    Add like, dislike, update, finadAll,

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -3,9 +3,10 @@ import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticleEntity } from './entity/article.entity';
+import UserEntity from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ArticleEntity])],
+  imports: [TypeOrmModule.forFeature([ArticleEntity, UserEntity])],
   providers: [ArticleService],
   controllers: [ArticleController],
 })
